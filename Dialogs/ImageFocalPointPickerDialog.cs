fdd58b42ae6aa7ca9e4c5c5105e5ad8cdfdfec9c
Anindita Bhattacharya <anindita.bhattacharya@isobar.com>
2020-05-19 15:19:31 +0530

    no message

---
+++ Dialogs/ImageFocalPointPickerDialog.cs
@@ -0,0 +1,69 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Links.UrlBuilders;
+using Sitecore.Resources.Media;
+using Sitecore.Web;
+using Sitecore.Web.UI.Pages;
+using Sitecore.Web.UI.Sheer;
+using System;
+using System.Web.UI.WebControls;
+using ImageField = Sitecore.Data.Fields.ImageField;
+
+namespace ImageFocalPointPicker.Dialogs
+{
+    public class ImageFocalPointPickerDialog : DialogForm
+    {
+        private const string Separator = "|";
+
+        private readonly Database _masterDb = Factory.GetDatabase("master");
+
+        public Sitecore.Web.UI.HtmlControls.Image ImageFrame;
+
+        public Sitecore.Web.UI.HtmlControls.Edit TextBoxCoordinate;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (ImageFrame == null) return;
+
+            TextBoxCoordinate.Value = WebUtil.GetQueryString("value");
+            string containerId = WebUtil.GetQueryString("containerId");
+            string fieldId = WebUtil.GetQueryString("fieldId");
+            Item currentItem = _masterDb.Items.GetItem(containerId);
+
+            ImageField imageField = currentItem.Fields[fieldId];
+
+            if (imageField == null || string.IsNullOrWhiteSpace(imageField.Value))
+            {
+                ImageFrame.Alt = "No image available";
+                ImageFrame.Src = "#";
+                return;
+            }
+
+            string imageSrc = MediaManager.GetMediaUrl(
+                _masterDb.Items.GetItem(imageField.MediaID),
+                new MediaUrlBuilderOptions
+                {
+                    Database = _masterDb,
+                    DisableMediaCache = true,
+                    DisableBrowserCache = true,
+                    AllowStretch = false
+                });
+
+            if (!string.IsNullOrWhiteSpace(imageSrc))
+            {
+                imageSrc += "&usecustomfunctions=1&centercrop=1";
+            }
+
+            ImageFrame.Src = imageSrc;
+        }
+
+        protected override void OnOK(object sender, EventArgs args)
+        {
+            SheerResponse.SetDialogValue(this.TextBoxCoordinate.Value);
+            base.OnOK(sender, args);
+        }
+    }
+}
---
+++ Fields/ImageFocalPointPickerField.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Specialized;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Links;
+using Sitecore.Resources.Media;
+using Sitecore.SecurityModel;
+using Sitecore.Shell.Applications.ContentEditor;
+using Sitecore.Web.UI.Sheer;
+
+namespace ImageFocalPointPicker.Fields
+{
+    public class ImageFocalPointPickerField : Image
+    {
+        public override void HandleMessage(Message message)
+        {
+            if (message["id"] != this.ID)
+            {
+                base.HandleMessage(message);
+            }
+
+            string name = message.Name;
+
+            if (name == "contentimage:focalpoint")
+            {
+                string onFocus = this.Attributes["onfocus"];
+
+                Sitecore.Context.ClientPage.Start(
+                    this,
+                    "PickFocalPoint",
+                    new NameValueCollection
+                    {
+                        {"containerId", onFocus.Split('?')[0].Split('{')[1].Replace("}", string.Empty)},
+                        {
+                            "fieldId",
+                            onFocus.Substring(onFocus.IndexOf("fld", StringComparison.Ordinal) + 5).Substring(0, 36)
+                        }
+                    });
+            }
+            else
+            {
+                base.HandleMessage(message);
+            }
+        }
+
+        protected void PickFocalPoint(ClientPipelineArgs args)
+        {
+            if (args.IsPostBack)
+            {
+                if (args.HasResult && args.Result != this.Value)
+                {
+                    this.SetModified();
+                    string attribute = this.XmlValue.GetAttribute("mediaid");
+                    if (string.IsNullOrEmpty(attribute))
+                    {
+                        SheerResponse.Alert("Select an image from the Media Library first.");
+                    }
+                    Item obj = Client.ContentDatabase.GetItem(attribute);
+                    obj.Editing.BeginEdit();
+                    obj["description"] = args.Result;
+                    obj.Editing.EndEdit();
+                }
+            }
+            else
+            {
+                string attribute = this.XmlValue.GetAttribute("mediaid");
+                if (string.IsNullOrEmpty(attribute))
+                {
+                    SheerResponse.Alert("Select an image from the Media Library first.");
+                }
+                else
+                {
+                    Item obj = Client.ContentDatabase.GetItem(attribute);
+                    if (obj == null)
+                        return;
+
+                    // Optional check to display number of instances this image is used - since the update is being made to media item
+
+                    /* ItemLink[] referrers = Globals.LinkDatabase.GetReferrers(obj);
+                    if (referrers != null && referrers.Length > 1)
+                    {
+                        SheerResponse.Confirm(
+                            $"This media item is referenced by {(object)referrers.Length} other items.\n\nEditing the media item will change it for all the referencing items.\n\nAre you sure you want to continue?");
+                        args.WaitForPostBack();
+                        return;
+                    } */
+
+                    string uri =
+                        $"{Sitecore.UIUtil.GetUri("control:ImageFocalPointPickerDialog")}&value={obj["Description"]}&containerId={args.Parameters["containerId"]}&fieldId={args.Parameters["fieldId"]}";
+                    SheerResponse.ShowModalDialog(uri, "500", "500", string.Empty, true);
+                    args.WaitForPostBack();
+                }
+            }
+        }
+    }
+}
